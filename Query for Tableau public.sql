4c37ddb85ac44bf61dbbf38ccf63afeda23a3b7e
Abdul ajees Shajahan <44908635+Abdulajees-123@users.noreply.github.com>
2023-02-16 00:24:09 +0400

    Add files via upload

---
+++ Query for Tableau public.sql
@@ -0,0 +1,50 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+
+SELECT SUM(new_cases) AS Total_cases,SUM(CAST(new_deaths AS int)) AS Total_Deaths,
+SUM(CAST(new_deaths AS int))/SUM(new_cases)*100 as DeathPercentage
+FROM CovidDeath									
+WHERE 
+--location like '%emirates%' AND
+location NOT IN('High income','Upper middle income')
+AND continent IS NOT NULL
+--GROUP BY date
+ORDER BY 1,2
+GO
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+SELECT location,SUM(CAST(total_deaths AS int)) AS Total_Death_Count
+from CovidDeath
+WHERE continent IS NULL
+AND location NOT IN('world','European Union','International','High income','Upper middle income','Low income')
+GROUP BY location
+ORDER BY Total_Death_Count DESC
+
+GO
+
+--3
+
+SELECT location,population,MAX(total_cases) AS HighestinfectionCount, MAX((total_cases/population)*100) AS PercentagePoulationInfected
+FROM CovidDeath
+GROUP BY location,population
+ORDER BY PercentagePoulationInfected DESC
+
+--4
+
+SELECT location,population,date,MAX(total_cases) AS HighestinfectionCount, MAX((total_cases/population)*100) AS PercentagePoulationInfected
+FROM CovidDeath
+GROUP BY location,population,date
+ORDER BY PercentagePoulationInfected DESC
+
+
+select distinct location from CovidDeath
